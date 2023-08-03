ab0b201257da390360a88b78a8c90af60351358e
ItsTMitts <98583941+LilBunnnnyyy@users.noreply.github.com>
2023-08-03 16:53:07 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,9 @@
+function br(){
+    window.location="index1.html";
+}
+function lr(){
+    window.location="index3.html";
+}
+function d(){
+    window.location="index2.html";
+}
---
+++ main3.js
@@ -0,0 +1,44 @@
+var ob=[];
+stat="";
+
+
+function preload(){
+li=loadImage('living room.jpg');
+}
+function setup(){
+    canvas=createCanvas(562,600);
+    canvas.center();
+    x=ml5.objectDetector('cocossd', ml);
+    document.getElementById("status").innerHTML="status: detecting object";
+}
+function ml(){
+    console.log("model loaded");
+    stat=true;
+    x.detect(li,gr);
+}
+function gr(error,result){
+if(error){
+console.log(error);
+}
+else{
+    console.log(result);
+    ob=result;
+}
+}
+function draw(){
+image(li,0,0,562,600);
+if(stat!=""){
+    for (let i = 0; i < ob.length; i++) {
+        document.getElementById("status").innerHTML="status: object detected";      
+fill("green");
+noFill();
+stroke("red");
+percent=floor(ob[i].confidence*100);
+text(ob[i].label+" "+percent+"%",ob[i].x+15,ob[i].y+15);
+rect(ob[i].x,ob[i].y,ob[i].width,ob[i].height);
+}
+}
+}
+function bb(){
+    window.location="index.html";
+}
